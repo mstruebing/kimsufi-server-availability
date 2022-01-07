ef648291d921b84adfe50a13d840baa0e8eb5df1
Max Strübing <mxstrbng@gmail.com>
2022-01-07 15:35:55 +0100

    feat: send mail if server is available

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
 #!/usr/bin/env node
 
 import fetch from "node-fetch";
+import nodemailer from "nodemailer";
 
-const KS1 = "2201sk010";
-const KS2 = "2201sk011";
+const WANTED_SERVERS = [
+  "2201sk010", // KS1
+  "2201sk011", // KS2
+];
+
+const SMTP_USER = process.env.SMTP_USER;
+const SMTP_PASS = process.env.SMTP_PASS;
+const RECEIVER = process.env.RECEIVER;
+
+const smtpTransport = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: SMTP_USER,
+    pass: SMTP_PASS,
+  },
+});
+
+const mailOptions = {
+  from: SMTP_USER,
+  to: RECEIVER,
+  subject: "KIMSUFI SERVER AVAILABLE",
+  text: "GO BUY",
+};
 
 (async () => {
   const response = await fetch(
     "https://www.ovh.com/engine/api/dedicated/server/availabilities?country=de"
   );
 
-  const products = await response.json();
+  const allProducts = await response.json();
+  const targetProducts = allProducts.filter((product) =>
+    WANTED_SERVERS.includes(product.hardware)
+  );
 
-  const ks1 = products.filter((product) => product.hardware === KS1);
-  const ks2 = products.filter((product) => product.hardware === KS2);
-
-  const ks1Available = ks1.some(
+  const available = targetProducts.some(
     (server) =>
       server.datacenters.filter(
         (datacenter) => datacenter.availability !== "unavailable"
       ).length > 0
   );
 
-  const ks2Available = ks2.some(
-    (server) =>
-      server.datacenters.filter(
-        (datacenter) => datacenter.availability !== "unavailable"
-      ).length > 0
-  );
-
-  console.log("ks1Available:", ks1Available);
-  console.log("ks2Available:", ks2Available);
+  if (available) {
+    smtpTransport.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("sending mail");
+        smtpTransport.sendMail(mailOptions);
+      }
+    });
+  }
 })();
